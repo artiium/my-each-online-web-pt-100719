ab54cb55b67076ac5f21c843363e497a884d972a
Artiium Arbyummi <artiium21@gmail.com>
2019-10-17 01:55:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,25 +8,4 @@ i = 0
     array
 end
 
-def hello_t(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
-  else
-    puts "Hey! No block was given!"
-  end
-end
 
-
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
